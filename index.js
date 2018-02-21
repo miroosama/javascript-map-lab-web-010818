78cf2f61ed3b648c3c13dd26f0329c07adc2fd1d
Osama Miro <o.miro650@gmail.com>
2018-02-20 21:51:35 -0500

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,19 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+
+
+const sortedRobots = robots.map(function (robot) {
+  const decepticonOrNot = knownDecepticons.includes(robot.name);
+  return Object.assign({}, robot, {
+    alliance: decepticonOrNot ? 'decepticon' : 'autobot'
+  });
+});
+
+const coloredZebraStripes = zebraStripes.map(function (stripe, index){
+  const blackStripe = (index % 2) === 0
+    return Object.assign({}, stripe, {
+      color: blackStripe? "black":"white"
+    })
+})
